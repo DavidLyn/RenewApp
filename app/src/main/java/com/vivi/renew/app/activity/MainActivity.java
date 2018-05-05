ba939ad921d4643e3a4971d75bbfb9de508dcee6
Lvvv <11437223@qq.com>
2018-05-05 21:58:25 +0800

    Initial commit

---
+++ app/src/main/java/com/vivi/renew/app/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.vivi.renew.app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vivi.renew.app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
+
---
+++ app/src/main/java/com/vivi/renew/app/test/Test.java
@@ -0,0 +1,8 @@
+package com.vivi.renew.app.test;
+
+/**
+ * Created by lvweiwei on 18/5/5.
+ */
+
+public class Test {
+}
