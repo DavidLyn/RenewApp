0421abd79150d0f48d26193d8bbd19848336a88d
Lvvv <11437223@qq.com>
2018-05-21 23:36:58 +0800

    add retrofit+RxJava test

---
+++ app/src/main/java/com/vivi/renew/app/test/activity/TestLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.vivi.renew.app.test.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vivi.renew.app.R;
+
+public class TestLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_layout);
+    }
+}
